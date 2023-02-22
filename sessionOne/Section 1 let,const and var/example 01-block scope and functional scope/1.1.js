db2fd0e0ae97750cd521bf91259b8642e1a610ba
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-02-22 20:02:30 +0530

    block scope and functional scope
    This code will result in a ReferenceError.
    
    The reason for this is that when using the `let` keyword to declare the variable `a` within the block `if (true)`, the scope of `a` is limited to that block. As a result, attempting to access `a` outside of the block will result in a `ReferenceError`.
    
    If the `let` keyword were changed to `var`, the code would run without an error because `var` has function scope rather than block scope. In that case, the output would be `2`

---
+++ sessionOne/Section 1 let,const and var/example 01-block scope and functional scope/1.1.js
@@ -0,0 +1,13 @@
+if(true){
+    let a=2;
+    // var a=2;
+}
+console.log(a);
+
+// function understandingScope() {
+//     if(true){
+//         let a=2;
+//     }
+//     console.log(a);
+// }
+// understandingScope();
