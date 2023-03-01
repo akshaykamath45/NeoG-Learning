8d130e8105c5de3565bfd37d765f283a7e64a77c
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-03-02 02:48:28 +0530

    Q8
    Write an ES6 function that takes an array of objects with the properties name and age, and returns an array with only the objects that have an age greater than 30.

---
+++ Functional Programming/Practice Set 1/8.js
@@ -0,0 +1,10 @@
+const filterByAge=(people)=>people.filter((peoples)=>peoples.age>30);
+
+const people = [
+  { name: 'Rahul', age: 25 },
+  { name: 'Raj', age: 35 },
+  { name: 'Vijay', age: 45 }
+];
+
+console.log(filterByAge(people)); 
+// Output: [{ name: 'Raj', age: 35 }, { name: 'Vijay', age: 45 }]
