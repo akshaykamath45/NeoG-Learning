143da06c583e6f69c5edbfde3b02c2518aae2698
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-03-03 19:19:39 +0530

    Q3
    Write a function that takes a number as input and determines if it is positive or negative.

---
+++ ES6+/Practice Set 1/3.js
@@ -0,0 +1,5 @@
+const checkNum=(num)=>num>=0 ? console.log("Positive Number") : console.log("Negative Number")
+
+console.log(checkNum(9)) // Positive Number
+console.log(checkNum(-8)) // Negative Number
+console.log(checkNum(22)) // Positive Number
