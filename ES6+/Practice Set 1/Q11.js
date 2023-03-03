1106c56e94bb5550d3761da135f38a24bc1c1580
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-03-03 19:24:05 +0530

    Q11
    Console individual values of the product object using object destructuring.

---
+++ ES6+/Practice Set 1/Q11.js
@@ -0,0 +1,11 @@
+const product = {
+    title: "iPhone",
+    price: 5999,
+    description: "The iPhone is a smartphone developed by Apple"
+  }
+  
+  const {title,price,description}=product;//Object Destrucuring
+  
+  console.log(title); // iPhone
+  console.log(price); // 5999
+  console.log(description); // The iPhone is a smartphone developed by Apple
