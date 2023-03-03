41b9dd7984fee5b06e06c30afcfee44413cfee86
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-03-03 19:20:26 +0530

    Q4
    Write a function that takes a number as input and determines if it is even or odd.

---
+++ ES6+/Practice Set 1/4.js
@@ -0,0 +1,5 @@
+const isEvenOdd=(num)=>num%2===0 ? console.log("Even Number") : console.log("Odd Number");
+
+console.log(isEvenOdd(5)) // Odd Number
+console.log(isEvenOdd(8)) // Even Number
+console.log(isEvenOdd(10)) // Even Number
