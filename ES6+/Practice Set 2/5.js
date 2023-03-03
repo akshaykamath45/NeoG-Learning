ba1ea2fd0379a351f5c9f9b16e402e4b2834cca4
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-03-03 19:34:30 +0530

    Q5
    Swap the values of two variables using array destructuring.

---
+++ ES6+/Practice Set 2/5.js
@@ -0,0 +1,21 @@
+let a = 1;
+let b = 2;
+
+[a,b]=[b,a];
+console.log(a) // 2
+console.log(b) // 1
+//Another Approach
+// let a = 1;
+// let b = 2;
+
+// const swap=(a,b)=>{
+// 	let temp;
+// 	temp=a
+// 	a=b;
+// 	b=temp;
+// 	}
+// swap(a,b)
+
+
+// console.log(a) // 2
+// console.log(b) // 1
