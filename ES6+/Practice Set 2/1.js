887e9420ec7349f1d6a70b6f6226a4871ff24278
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-03-03 19:32:20 +0530

    Q1
    Create an object person with two properties, "name" and "age" and then updates the "age" property to a new value. Initial age should be 30.

---
+++ ES6+/Practice Set 2/1.js
@@ -0,0 +1,13 @@
+let age=30;
+const person={name,age};
+console.log(person);
+person.age=35;
+
+const person = {
+	name: "Uppal",
+	age: 30
+}
+
+person.age = 35
+
+console.log(person.age); // Output: 35
