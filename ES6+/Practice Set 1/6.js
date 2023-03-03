520bf60cb3ecefe067d5454d3174aadb40b50bd2
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-03-03 19:21:26 +0530

    Q6
    Write a function that takes a string as input and determines if it is longer than 5 characters.

---
+++ ES6+/Practice Set 1/6.js
@@ -0,0 +1,6 @@
+const checkLength=(string)=>{
+    string.length > 5 ? console.log("more than 5 characters") : console.log("less than 5 characters")
+  }
+  
+  console.log(checkLength("Programming")) // more than 5 characters 
+  console.log(checkLength("Jeep")) // less than 5 characters
