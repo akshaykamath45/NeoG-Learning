4e3d192349ebe98170985bf21c36672dd32afcc9
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-03-03 19:23:08 +0530

    Q9
    Write a function that takes a number as input and determines if it is a multiple of 3.

---
+++ ES6+/Practice Set 1/9.js
@@ -0,0 +1,11 @@
+function isMultipleOfThree(num){
+    if(num%3===0){
+      console.log("true");
+    }
+    else{
+      console.log("false");
+    }
+  }
+  
+  console.log(isMultipleOfThree(5)); // false
+  console.log(isMultipleOfThree(9)); // true
