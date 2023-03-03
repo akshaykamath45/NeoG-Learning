ce835ed2c15c2393699f7c839b2ad3be9a898b35
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-03-03 19:21:52 +0530

    Q7
    Write a function that takes a number as input and determines if it is between 1 and 10.

---
+++ ES6+/Practice Set 1/7.js
@@ -0,0 +1,11 @@
+const isBetweenOneAndTen=(num)=>{
+    if(num>1 && num<10){
+      console.log("true");
+    }
+  else{
+    console.log("false");
+  }
+}
+
+console.log(isBetweenOneAndTen(5)); // true
+console.log(isBetweenOneAndTen(11)); // false
