ae8926a6ac05b23a17ac5281b6ea11dc1c2ef44a
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-03-03 19:33:11 +0530

    Q3
    Write a function that takes an object person and a number num as arguments and returns true if the person's age plus num is greater than 21. Otherwise, it should return false.

---
+++ ES6+/Practice Set 2/3.js
@@ -0,0 +1,6 @@
+const isEligible=(person,num)=>(person.age+num)>21 ? "true" : "false"
+
+
+const person1 = { name: 'Ajay', age: 20 };
+console.log(isEligible(person1, 1)); // false
+console.log(isEligible(person1, 2)); // true
