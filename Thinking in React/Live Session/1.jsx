7ebdce528060b42fe169c26c2e96d52b60daf848
Akshay Kamath <akshaykamath193@gmail.com>
2023-04-01 03:32:22 +0530

    inline style

--- Thinking in React/Live Session/1.jsx
+++ Thinking in React/Live Session/1.jsx
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1 className="app-header">Sum of Two Numbers</h1>
+      <h1 className="app-header" style={{color:"green"}}>Sum of Two Numbers</h1>
       <div className="app-body">
         <p>First Number :5</p>
         <p>Second Number :4</p>
