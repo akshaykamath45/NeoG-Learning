afb9c26df22ceefad38acd2f558ded026065fdb7
Akshay Kamath <akshaykamath193@gmail.com>
2023-04-01 03:24:19 +0530

    Q2
    Create an EmployeeCard component to display name, designation and work experience. Import the file in App.js file and render data on DOM

---
+++ Thinking in React/Live Session/2.jsx
@@ -0,0 +1,12 @@
+export const EmployeeCard = () => {
+    return (
+      <div>
+        <h1 className="app-header">Akshay Kamath</h1>
+        <div className="app-body">
+          <p>Designation: Computer Engineering Student</p>
+          <p>Year : 2nd Year</p>
+        </div>
+      </div>
+    );
+  };
+  
