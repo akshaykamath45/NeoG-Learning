94828f9fcc91ee020979160abdb8ac2ec7b64b15
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-04-01 04:02:19 +0530

    change color,font using span tag
    In EmployeeCard,
    1.change the color of “Designation:” to red.
    2.change the color of “Experience:” to blue.
    3.make the font size of “10 years” to 18px

--- Thinking in React/Live Session/2.jsx
+++ Thinking in React/Live Session/2.jsx
@@ -1,12 +1,31 @@
-export const EmployeeCard = () => {
-    return (
-      <div>
-        <h1 className="app-header">Akshay Kamath</h1>
-        <div className="app-body">
-          <p>Designation: Computer Engineering Student</p>
-          <p>Year : 2nd Year</p>
-        </div>
-      </div>
-    );
-  };
+// export const EmployeeCard = () => {
+//     return (
+//       <div>
+//         <h1 className="app-header">Akshay Kamath</h1>
+//         <div className="app-body">
+//           <p>Designation: Computer Engineering Student</p>
+//           <p>Year : 2nd Year</p>
+//         </div>
+//       </div>
+//     );
+//   };
   
+const changeColorDesignation={color:"red"}
+export const EmployeeCard = () => {
+  return (
+    <div>
+      <h1 className="app-header">Akshay Kamath</h1>
+      <div className="app-body">
+        <p><span style={changeColorDesignation}>Designation:</span> Computer Engineering Student</p>
+        <p><span style={{color:"blue"}}> Experience: </span> 3 months</p> 
+        <p>Year : <span style={{fontSize:"18px"}}>2nd Year</span></p>
+      </div>
+    </div>
+  );
+};
+
+
+// In EmployeeCard,
+// 1.change the color of “Designation:” to red.
+// 2.change the color of “Experience:” to blue.
+// 3.make the font size of “10 years” to 18px
