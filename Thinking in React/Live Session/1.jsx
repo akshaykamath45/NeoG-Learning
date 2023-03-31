39f46c481101ed1fda8417af3bcef09e9ee68b0b
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-04-01 04:18:09 +0530

    making use of props

--- Thinking in React/Live Session/1.jsx
+++ Thinking in React/Live Session/1.jsx
@@ -1,14 +1,20 @@
-const setFontSize={fontSize:"20px"}
+const setFontSize = { fontSize: "20px" };
+// const firstNumber = 4;
+// const secondNumber = 5;
 
-export const SumCard = () => {
+export const SumCard = ({firstNumber,secondNumber}) => {
   return (
     <div>
-      <h1 className="app-header" style={{color:"green"}}>Sum of Two Numbers</h1>
+      <h1 className="app-header" style={{ color: "green" }}>
+        Sum of Two Numbers
+      </h1>
       <div className="app-body">
-        <p style={setFontSize}>First Number :5</p>
-        <p style={setFontSize}>Second Number :4</p>
-        <p style={setFontSize}>Sum: 9</p>
+        <p style={setFontSize}>First Number :{firstNumber}</p>
+        <p style={setFontSize}>Second Number :{secondNumber}</p>
+        <p style={setFontSize}>Sum: {firstNumber + secondNumber}</p>
       </div>
     </div>
   );
 };
+
+// <SumCard firstNumber={4} secondNumber={5} />
