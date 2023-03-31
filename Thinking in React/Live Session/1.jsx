9b0c7b4844add30f6ed3bbfd6d8334c930ffdf7f
Akshay Kamath <akshaykamath193@gmail.com>
2023-04-01 03:15:51 +0530

    Q1
    Write a React component to display two numbers and the sum of those two numbers.

---
+++ Thinking in React/Live Session/1.jsx
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className="app-header">Sum of Two Numbers</h1>
+      <div className="app-body">
+        <p>First Number :5</p>
+        <p>Second Number :4</p>
+        <p>Sum: 9</p>
+      </div>
+    </div>
+  );
+}
