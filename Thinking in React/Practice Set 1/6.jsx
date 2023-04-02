84ff0ae28a8dea5392a04e920d9fc59b65ca92a2
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-04-03 02:30:13 +0530

    Q6
    Consider the products data from previous question and display all the product details as unordered list for which the number of sales is more than the quantity.

---
+++ Thinking in React/Practice Set 1/6.jsx
@@ -0,0 +1,28 @@
+const products = [
+    { name: 'Perk', quantity: 10, sales: 7 },
+    { name: 'Pepsi', quantity: 10, sales: 20 },
+    { name: 'Coke', quantity: 18, sales: 50 },
+    { name: 'Maggi', quantity: 41, sales: 22 },
+    { name: '5Star', quantity: 7, sales: 9 },
+  ]
+  
+  
+  function ProductInformation({product}){
+    return(
+      <ul>
+        <h1>Product Names</h1>
+        {
+          product.map(({name,quantity,sales})=><li>
+            Name : {name}, Quantity : {quantity}, Sales :{sales}
+          </li>)
+        }
+      </ul>
+    )
+  }
+  export default function App() {
+    return (
+      <div className="App">
+        <ProductInformation product={products}/>
+      </div>
+    );
+  }
