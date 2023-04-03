8bc8e933a84b5a96f33db0c122a5c7537b31fae4
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-04-04 01:46:23 +0530

    Q9
    Write an ES6 function to return the second element of the given array by adding “Hello” before it.

---
+++ ES6+/Practice Set 3/9.js
@@ -0,0 +1,9 @@
+const sayHello=(arr)=>{
+    return `Hello ${arr[1]}`;
+    }
+    
+    console.log(sayHello(['Akshay', 'Sweta', 'Prerana', 'Vinay']))
+    // Hello Sweta
+    
+    console.log(sayHello(['Kanika', 'Rakesh', 'Prerana', 'Puja']))
+    // Hello Rakesh
