0712dbbdcd4f80f907e4e5a11669bd7fff39348d
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-04-04 01:50:45 +0530

    Q11
    Write an ES6 function to return the sum of only first 2 elements of the array .

---
+++ ES6+/Practice Set 3/Q11.js
@@ -0,0 +1,5 @@
+const sumFirstTwoElements=(arr)=>{
+    return arr[0]+arr[1];
+  }
+  console.log(sumFirstTwoElements([10, 4, 3, 7, 1, 5])) // 14
+  console.log(sumFirstTwoElements([12, 14, 3, 27, 15])) // 26
