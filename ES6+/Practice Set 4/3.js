ab5730b38eb4a56a8ec420b961dcfe8c4539bec7
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-04-04 02:21:55 +0530

    Q3
    Write an ES6 function that takes an array of numbers and converts even numbers to odd numbers by adding 1 to that number.

---
+++ ES6+/Practice Set 4/3.js
@@ -0,0 +1,11 @@
+const convertEvenToOdd=(arr)=>{
+    for(let i=0;i<arr.length;i++){
+      if(arr[i]%2===0){
+        arr[i]+=1;
+      }
+    }
+      return arr;
+  }
+  var numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  console.log(convertEvenToOdd(numArr))
+  // [1, 3, 3, 5, 5, 7, 7, 9, 9]
