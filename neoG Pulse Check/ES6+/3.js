037d477710d8f5d6fb399721e8536acdce5dc57c
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-04-04 03:54:04 +0530

    Q1
    Write a function that uses destructuring to extract the first two elements from an array, and returns them in an object with keys 'first' and 'second'.

---
+++ neoG Pulse Check/ES6+/3.js
@@ -0,0 +1,9 @@
+const findFirstAndSecond=(arr)=>{
+    let first=arr[0];
+    let second=arr[1];
+    const requiredObject={first,second};
+    return requiredObject;
+  }
+  
+  const arr=[1,2,3,4,5]
+  console.log(findFirstAndSecond(arr));
