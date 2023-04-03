50d0d8d0e264c56279705eb4ae4a3f05bf43d0a0
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-04-04 00:46:36 +0530

    Q5
    Convert the following code to ES6+ syntax with minimum number of characters.

---
+++ ES6+/Practice Set 3/5.js
@@ -0,0 +1,14 @@
+// function startsWithA(str) {
+//   if (str.charAt(0) === 'A') {
+//     return true
+//   } else {
+//     return false
+//   }
+//}
+const startsWithA=(str)=>{
+    return str.charAt(0)==='A'?true:false
+  }
+  console.log(startsWithA('Akshita'))
+  // true
+  console.log(startsWithA('Jeena'))
+  // false
