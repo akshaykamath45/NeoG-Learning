0ef6d573af20d628d99cbfe367fee204f6e6c984
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-04-04 01:49:33 +0530

    Q10
    Write an ES6 function to return sum of all numbers at even indices of the given array.

---
+++ ES6+/Practice Set 3/Q10.js
@@ -0,0 +1,11 @@
+const sumOfEvenIndices=(arr)=>{
+    let sum=0;
+    for(let i=0;i<arr.length;i++){
+      if(i%2===0){
+        sum=sum+arr[i];
+      }
+    }
+    return sum;
+  }
+  console.log(sumOfEvenIndices([2, 4, 3, 7, 1, 5])) // 6
+  console.log(sumOfEvenIndices([12, 14, 3, 27, 15, 25])) // 30
