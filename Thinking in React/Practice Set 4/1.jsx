e217048a1fc83713672436d4209f07bc0c1e5a85
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-04-06 00:48:54 +0530

    Q1
    Given an array of characters, build a Tab component in React that shows the name of characters on click of two buttons, one for heroes and other for villains. On click of “Show Heroes” button, a list of heroes should be displayed and on click of “Show Villains” button, a list of villains should be displayed on the DOM. Use useState Hook.

---
+++ Thinking in React/Practice Set 4/1.jsx
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import React from "react";
+
+const characters = {
+  heroes: [
+    {
+      name: "IRON MAN",
+      powers: "Genius,Rich,IRON Suit",
+      costume: "IRON MAN suit",
+    },
+    {
+      name: "ANT MAN",
+      powers: "Ant man suit",
+      costume: "Ant man suit",
+    },
+    {
+      name: "Spider-man",
+      powers: "Reflexes,Speed,Spider-Sense",
+      costume: "Spider Suit",
+    },
+    {
+      name: "Bat man",
+      powers: "Rich,Bat suit",
+      costume: "Bat Suit",
+    },
+    {
+      name: "Super-man",
+      powers: "Superhuman strength,Reflexes,Speed",
+      costume: "Superman  Suit",
+    },
+  ],
+  villains: [
+    {
+      name: "Thanos",
+      powers: "Superhuman strength,Reflexes,Speed,",
+      costume: "Metal armor",
+    },
+    {
+      name: "Venom",
+      powers: "shapeshifting and camouflage Symbiotes autonomous defense",
+      costume: "black suit",
+    },
+    {
+      name: "Kang the conqueror",
+      powers: "time travel, can access all tech",
+      costume: "kang armor",
+    },
+    {
+      name: "Joker",
+      powers: "clownlike appearance and sick humour",
+      costume: "Joker costume",
+    },
+  ],
+};
+
+function Tab({ objectInformation }) {
+  const [display, setDisplay] = useState(null);
+
+  function displayHeroes() {
+    setDisplay(
+      <ol>
+        {objectInformation.heroes.map(({ name }) => (
+          <li>{name}</li>
+        ))}
+      </ol>
+    );
+  }
+  function displayVillains() {
+    setDisplay(
+      <ol>
+        {objectInformation.villains.map(({ name }) => (
+          <li>{name}</li>
+        ))}
+      </ol>
+    );
+  }
+  return (
+    <div>
+      <button onClick={displayHeroes}>Show Heroes </button>
+      <button onClick={displayVillains}>Show Villains</button>
+      {display}
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <Tab objectInformation={characters} />
+    </div>
+  );
+}
