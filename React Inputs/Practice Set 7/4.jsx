40ef767b56cd9db03874562b56279e9008f68b35
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-05-03 02:45:18 +0530

    Q4
    Create a React component that fetches a list of users from an API endpoint using useEffect hook and displays the name, email, and website of each user on the screen using the useState hook. Add a dropdown which filters the users by company name.
    
    Codesandbox : https://codesandbox.io/s/q3-ps7-forked-1jugei?file=/src/App.jsx:0-1402
    
    Working Link : https://1jugei.csb.app/

--- React Inputs/Practice Set 7/4.jsx
+++ React Inputs/Practice Set 7/4.jsx
@@ -1,73 +1,57 @@
+import "./styles.css";
 import { fakeFetch } from "./api/fakefetch";
 import { useState, useEffect } from "react";
 
 export default function App() {
-  const [userData, setUserData] = useState({});
-  const [showAddress, setShowAddress] = useState(false);
-  const [hideAddress, setHideAddress] = useState(false);
-  const [showAddressButton, setShowAddressButton] = useState(true);
-  const [hideAddressButton, setHideAddressButton] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [userInput, setUserInput] = useState("");
+
   const getUserData = async () => {
-    setIsLoading(true);
     try {
-      const { data, status } = await fakeFetch("https://example.com/api/user");
+      const { data, status } = await fakeFetch("https://example.com/api/users");
       if (status === 200) {
-        setUserData(data);
+        setUsers(data);
       }
-      setIsLoading(false);
     } catch (e) {
       console.log(e);
     }
   };
-  const { name, email, phone, address } = userData;
+
   useEffect(() => {
     getUserData();
   }, []);
 
-  const displayAddress = () => {
-    setShowAddress(true);
-    setHideAddress(true);
-    setShowAddressButton(false);
-    setHideAddressButton(true);
+  const handleInput = (e) => {
+    setUserInput(e.target.value);
   };
-  const removeAddress = () => {
-    setShowAddress(false);
-    setHideAddress(true);
-    setShowAddressButton(true);
-    setHideAddressButton(false);
-    return <div></div>;
-  };
-  if (isLoading) {
-    return <div>Loading Data...</div>;
-  }
+
   return (
     <div>
-      <h1>User</h1>
-      <br />
-      Name: {name} <br />
-      Email: {email} <br />
-      Phone: {phone}
-      <br />
-      {showAddressButton && (
-        <button onClick={displayAddress}>Show Address</button>
-      )}
-      {hideAddress && hideAddressButton && (
-        <button onClick={removeAddress}>Hide Address</button>
-      )}
-      {showAddress && (
-        <div>
-          <br />
-          {address.street} <br />
-          {address.suite}
-          <br />
-          {address.city}
-          <br />
-          {address.zipcode}
-          <br />
-        </div>
-      )}
-      <div></div>
+      <h1>Users</h1>
+      Filter by Year:
+      <select onChange={handleInput}>
+        <option value="">All Companies</option>
+        <option value="XYZ Corp">XYZ Corp</option>
+        <option value="ABC Inc">ABC Inc</option>
+        <option value="ACME Corp">ACME Corp</option>
+      </select>
+      {users &&
+        users
+          .filter((user) => {
+            if (userInput === "") {
+              return true;
+            } else {
+              return user.company.toString() === userInput;
+            }
+          })
+          .map(({ name, email, website, company }) => (
+            <li key={name}>
+              <p>{name}</p>
+              <p>{email}</p>
+              <p>{website}</p>
+              <p>{company}</p>
+            </li>
+          ))}
     </div>
   );
 }
@@ -75,28 +59,49 @@ export default function App() {
 export const fakeFetch = (url) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (url === "https://example.com/api/user") {
+      if (url === 'https://example.com/api/users') {
         resolve({
           status: 200,
-          message: "Success",
-          data: {
-            name: "John Doe",
-            email: "john.doe@example.com",
-            phone: "+1 555-555-5555",
-            address: {
-              street: "123 Main St",
-              suite: "Suite 456",
-              city: "Anytown",
-              zipcode: "12345",
+          message: 'Success',
+          data: [
+            {
+              name: 'John Doe',
+              email: 'john@example.com',
+              website: 'example.com',
+              company: 'ABC Inc',
             },
-          },
-        });
+            {
+              name: 'Jane Doe',
+              email: 'jane@example.com',
+              website: 'example.com',
+              company: 'XYZ Corp',
+            },
+            {
+              name: 'Bob Smith',
+              email: 'bob@example.com',
+              website: 'example.com',
+              company: 'ABC Inc',
+            },
+            {
+              name: 'Alice Brown',
+              email: 'alice@example.com',
+              website: 'example.com',
+              company: 'ACME Corp',
+            },
+            {
+              name: 'Charlie Green',
+              email: 'charlie@example.com',
+              website: 'example.com',
+              company: 'XYZ Corp',
+            },
+          ],
+        })
       } else {
         reject({
           status: 404,
-          message: "User not found.",
-        });
+          message: 'Users list not found.',
+        })
       }
-    }, 2000);
-  });
-};
+    }, 2000)
+  })
+}
