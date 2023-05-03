6e228cf361ac65e2d5b2ffd07c3f036c118e2a98
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-05-03 17:18:04 +0530

    Q10
    Adding on to the previous question, Add a search bar to the component that allows users to filter the products by name. The search bar should update the list of displayed products in real-time as the user types. The search functionality should be case-insensitive
    
    Codesandbox : https://codesandbox.io/s/q10-ps7-b8u2wn?file=/src/App.jsx:0-1717
    
    Wokring Link : https://b8u2wn.csb.app/

---
+++ React Inputs/Practice Set 7/10.jsx
@@ -0,0 +1,91 @@
+import "./styles.css";
+import { fakeFetch } from "./api/fakefetch";
+import { useState, useEffect } from "react";
+
+export default function App() {
+  const [products, setProducts] = useState([]);
+  const [displaySearchProduct, setDisplaySearchProduct] = useState([]);
+  const [userInput, setUserInput] = useState("");
+
+  const getProductsData = async () => {
+    try {
+      const { data, status } = await fakeFetch(
+        "https://example.com/api/products"
+      );
+      if (status === 200) {
+        setProducts(data.products);
+        setDisplaySearchProduct(data.products);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getProductsData();
+  }, []);
+
+  const handleUserInput = (e) => {
+    const input = e.target.value.toLowerCase();
+    setUserInput(input);
+    const filteredProducts = products.filter((product) =>
+      product.name.toLowerCase().includes(userInput)
+    );
+    setDisplaySearchProduct(filteredProducts);
+  };
+
+  return (
+    <div>
+      <h1> Products </h1>
+      <input placeholder="Search Products" onChange={handleUserInput}></input>
+      {displaySearchProduct.length === 0 ? (
+        <p>No products found</p>
+      ) : (
+        displaySearchProduct.map(({ name, price, quantity, rating }) => (
+          <li
+            key={name}
+            style={{
+              border: "1px solid black",
+              borderRadius: "4px",
+              padding: "2px",
+              margin: "8px",
+              listStyle: "none",
+            }}
+          >
+            <p>
+              {name} Rating: {rating}{" "}
+            </p>
+            <p>Price: {price}</p>
+            <p>Quantity: {quantity}</p>
+          </li>
+        ))
+      )}
+    </div>
+  );
+}
+
+const fakeFetch = (url) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (url === "https://example.com/api/products") {
+        resolve({
+          status: 200,
+          message: "Success",
+          data: {
+            products: [
+              { name: "Color Pencils", price: 50, quantity: 40, rating: 4.5 },
+              { name: "Sketchpens", price: 110, quantity: 20, rating: 3.8 },
+              { name: "Eraser", price: 20, quantity: 20, rating: 4.2 },
+              { name: "Sharpener", price: 22, quantity: 30, rating: 4.7 },
+            ],
+          },
+        });
+      } else {
+        reject({
+          status: 404,
+          message: "Product list not found.",
+        });
+      }
+    }, 2000);
+  });
+};
