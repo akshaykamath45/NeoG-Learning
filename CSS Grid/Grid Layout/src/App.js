4551e39ed9fdb87a32a81bee5c172ff878146e5c
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-08-16 00:59:02 +0530

    Grid Session
    grid layout using grid template columns and rows
    
    Codesandbox : https://codesandbox.io/s/css-grid-session-78znyr
    
    Working Link : https://78znyr.csb.app/

---
+++ CSS Grid/Grid Layout/src/App.js
@@ -0,0 +1,21 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div class="layout">
+        <div className="card">lorem ipsum dolor set</div>
+        <div className="card">lorem ipsum dolor set</div>
+        <div className="card">lorem ipsum dolor set</div>
+
+        <div className="card">lorem ipsum dolor set</div>
+        <div className="card">lorem ipsum dolor set</div>
+        <div className="card">lorem ipsum dolor set</div>
+
+        <div className="card">lorem ipsum dolor set</div>
+        <div className="card">lorem ipsum dolor set</div>
+        <div className="card">lorem ipsum dolor set</div>
+      </div>
+    </div>
+  );
+}
