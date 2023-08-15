944629e198d98ceb3929332d8772c589a4793d41
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-08-15 23:26:01 +0530

    CSS Flex Session
    Codesandbox : https://codesandbox.io/s/css-flex-session-7h8mqx
    
    Working Link : https://7h8mqx.csb.app/

---
+++ CSS Flex/CSS Flex Session/src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <nav className="nav">
+      <li className="nav-item"> home </li>
+      <li className="nav-item"> about </li>
+      <li className="nav-item"> products </li>
+      <li className="nav-item"> cart </li>
+      <li className="nav-item"> wishlist </li>
+    </nav>
+  );
+}
