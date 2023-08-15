e1a11459426fd5800e71b5919562da9f91ff8385
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-08-16 01:02:31 +0530

    Responsivness using Media Query
    Mobile > Desktop > Tablet,while applying css
    
    by default for mobile
    min-width : 768 px for tablet
    min-width:1034 px for desktop
    
    Codesandbox : https://codesandbox.io/s/css-responsiveness-2jqhz6
    
    Working Link : https://2jqhz6.csb.app/

---
+++ CSS Grid/Responsiveness/src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Heading 1</h1>
+      <h2>Heading 2</h2>
+      <p>
+        Lorem ipsum fdesgrjbh efrshbdfm ewsdfkwjenm s sfes f
+        esfhjkfnrsdhjfrdsbmerf sdregfidsbhj,mesgr fd
+      </p>
+    </div>
+  );
+}
