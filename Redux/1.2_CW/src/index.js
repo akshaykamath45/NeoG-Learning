8dcefa47f6d72003f8571a78f6cea7918034a080
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-10-06 13:58:05 +0530

    ex06: interacting with the todo list

--- Redux/1.2_CW/src/index.js
+++ Redux/1.2_CW/src/index.js
@@ -7,3 +7,10 @@ const callMeWhenStateUpdates = () => {
     console.log(store.getState());
 };
 store.subscribe(callMeWhenStateUpdates);
+
+//action is just object
+//action creators are functions that return  action objects
+
+const addButton = document.getElementById("add");
+const todoInput = document.getElementById("todo-input");
+const todoList = document.getElementById("todo-list");
