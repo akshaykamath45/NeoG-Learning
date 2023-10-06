8537e127e3f5a89010d6bde0dd61faffd8de8464
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-10-06 10:42:26 +0530

    ex03: subscribe to the store
    Subscribing lets us know whenever something changes in our app's state. It's like getting a message whenever someone takes or adds a cookie to our cookie jar.

--- Redux/1.1_CW/src/index.js
+++ Redux/1.1_CW/src/index.js
@@ -1,4 +1,10 @@
-import { createStore } from "redux"
-import { cookiesReducer } from "./cookiesReducer"
+import { createStore } from "redux";
+import { cookiesReducer } from "./cookiesReducer";
 
-const store = createStore(cookiesReducer)
+const store = createStore(cookiesReducer);
+
+//whenever this a change in the store,please notify me
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
