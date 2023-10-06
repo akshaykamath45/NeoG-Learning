e95b23a725fbf6751ea89e43eb758cc59bf508e4
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-10-06 13:47:37 +0530

    ex03: subscribe to the store for todo list

--- Redux/1.2_CW/src/index.js
+++ Redux/1.2_CW/src/index.js
@@ -2,3 +2,8 @@ import { createStore } from "redux";
 import { todosReducer } from "./todosReducer";
 
 const store = createStore(todosReducer);
+
+const callMeWhenStateUpdates = () => {
+    console.log(store.getState());
+};
+store.subscribe(callMeWhenStateUpdates);
