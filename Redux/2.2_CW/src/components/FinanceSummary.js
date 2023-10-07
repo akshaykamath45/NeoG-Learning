0ab8b764a483f7b0232493ae97d49f604ca12ca5
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-10-08 02:11:26 +0530

    ex05: create a FinanceSummary component

---
+++ Redux/2.2_CW/src/components/FinanceSummary.js
@@ -0,0 +1,10 @@
+export const FinanceSummary = () => {
+    return (
+        <div>
+            <h2>Finance Summary</h2>
+            <p>Income: 3000rs</p>
+            <p>Expenses: 2000rs</p>
+            <p>Balance: 1000rs</p>
+        </div>
+    );
+};
