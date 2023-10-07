176182f4e43977ee99fb41ed6ec766bea336db40
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-10-08 01:52:26 +0530

    ex01: create actions

---
+++ Redux/2.2_CW/src/App.js
@@ -0,0 +1,11 @@
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+export default function App() {
+  // const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h3>Finance Management </h3>
+    </div>
+  );
+}
