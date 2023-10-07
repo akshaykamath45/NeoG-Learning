10de3df8b9737329a93837c8c6e665d5f7cf74aa
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-10-08 02:18:34 +0530

    ex06: connect components to redux

--- Redux/2.2_CW/src/components/IncomeExpenseForm.js
+++ Redux/2.2_CW/src/components/IncomeExpenseForm.js
@@ -1,6 +1,17 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addAmount, addExpense } from "../actions";
 export const IncomeExpenseForm = () => {
     const [amount, setAmount] = useState(0);
+    const dispatch = useDispatch();
+    const handleAddIncome = () => {
+        dispatch(addAmount(parseFloat(amount)));
+        setAmount(0);
+    };
+    const handleAddExpense = () => {
+        dispatch(addExpense(parseFloat(amount)));
+        setAmount(0);
+    };
     return (
         <div>
             <input
@@ -8,8 +19,8 @@ export const IncomeExpenseForm = () => {
                 value={amount}
                 onChange={(e) => setAmount(e.target.value)}
             />
-            <button>Add Income</button>
-            <button>Add Expense</button>
+            <button onClick={handleAddIncome}>Add Income</button>
+            <button onClick={handleAddExpense}>Add Expense</button>
         </div>
     );
 };
