a1adaad38118292db273cc21a53e92ce49be1193
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-10-08 01:34:27 +0530

    ex05: add handleClick

--- Redux/2.1_CW/src/App.js
+++ Redux/2.1_CW/src/App.js
@@ -1,7 +1,13 @@
 import { useSelector } from "react-redux";
 
 export default function App() {
-  let counter = useSelector((state) => state.count);
+    let counter = useSelector((state) => state.count);
 
-  return <div>Counter : {counter}</div>;
+    return (
+        <div>
+            <h3>Counter : {counter}</h3>
+            <button onClick={(e) => handleClick("add")}>+</button>
+            <button onClick={(e) => handleClick("minus")}>-</button>
+        </div>
+    );
 }
