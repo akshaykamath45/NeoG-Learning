3069d935d2dc0f7e0d3859b81e984ba7ca9bc79e
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-10-08 01:36:27 +0530

    ex06: dispatch actions

--- Redux/2.1_CW/src/App.js
+++ Redux/2.1_CW/src/App.js
@@ -1,8 +1,11 @@
 import { useSelector } from "react-redux";
-
+import { useDispatch } from "react-redux";
 export default function App() {
     let counter = useSelector((state) => state.count);
-
+    const dispatch = useDispatch();
+    const handleClick = (type) => {
+        dispatch({ type });
+    };
     return (
         <div>
             <h3>Counter : {counter}</h3>
