871bdc0d718f099daeec814da33b3397fb4decd7
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-10-08 02:21:41 +0530

    ex07: display finance summary

--- Redux/2.2_CW/src/components/FinanceSummary.js
+++ Redux/2.2_CW/src/components/FinanceSummary.js
@@ -1,10 +1,14 @@
+import { useSelector } from "react-redux";
+
 export const FinanceSummary = () => {
+    const income = useSelector((state) => state.income);
+    const expenses = useSelector((state) => state.expense);
     return (
         <div>
             <h2>Finance Summary</h2>
-            <p>Income: 3000rs</p>
-            <p>Expenses: 2000rs</p>
-            <p>Balance: 1000rs</p>
+            <p>Income: ${income}rs</p>
+            <p>Expenses: ${expenses}rs</p>
+            <p>Balance: ${income - expenses}rs</p>
         </div>
     );
 };
