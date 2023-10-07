c92b7b0d9888750dbeddcfd366805de4c8d111a1
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-10-07 15:16:03 +0530

    ex04: render product list

--- Redux/1.4_CW/src/index.js
+++ Redux/1.4_CW/src/index.js
@@ -6,3 +6,19 @@ const callMeWhenStateUpdates = () => {
     console.log(store.getState());
 };
 store.subscribe(callMeWhenStateUpdates);
+
+const products = [
+    { id: 1, name: "Product A", price: 10 },
+    { id: 2, name: "Product B", price: 20 },
+    { id: 3, name: "Product C", price: 15 }
+];
+
+const renderProducts = () => {
+    const productList = document.getElementById("product-list");
+    productList.innerHTML = products
+        .map((product) => {
+            return `<li>${product.name} - ${product.price}</li>`;
+        })
+        .join("");
+};
+renderProducts();
