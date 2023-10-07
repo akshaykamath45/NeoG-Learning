552a3dea56d7b0ee81c26fce2bfb5bfba27431cc
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-10-08 01:18:10 +0530

    ex01: create redux store
    creating counterReducer

---
+++ Redux/2.1_CW/src/counterReducer.js
@@ -0,0 +1,14 @@
+const initalState = { count: 0 };
+export const counterReducer = (state = initalState, action) => {
+    switch (action.type) {
+        case "add": {
+            return { ...state, count: state.count + 1 };
+        }
+        case "minus": {
+            return { ...state, count: state.count - 1 };
+        }
+        default: {
+            return state;
+        }
+    }
+};
