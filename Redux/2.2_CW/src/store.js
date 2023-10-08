0aa0fa45315db06ce5040538c31e648b4d4a6486
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-10-08 11:06:48 +0530

    finance management app using redux
    Codesandbox : https://codesandbox.io/s/rx2-2-cw-rq5yfn
    
    Working Link : https://rq5yfn.csb.app/

--- Redux/2.2_CW/src/store.js
+++ Redux/2.2_CW/src/store.js
@@ -1,4 +1,4 @@
 import { createStore } from "redux";
 import { financeReducers } from "./financeReducers";
 
-const store = createStore(financeReducers);
+export const store = createStore(financeReducers);
