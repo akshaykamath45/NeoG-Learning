2eea4c1f73b53a6e7967366249d7c200eb9bd1c7
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-10-08 11:32:30 +0530

    ex02: create the logging middleware
    loggerMiddleware is a Redux middleware that logs the current state, dispatched actions, and the new state after an action is processed.

---
+++ Redux/2.3_CW/src/loggerMiddleware.js
@@ -0,0 +1,6 @@
+export const loggerMiddleware = (store) => (next) => (action) => {
+    console.log("Current State : ", store.getState());
+    console.log("Action : ", action);
+    next(action);
+    console.log("New State : ", store.getState());
+};
