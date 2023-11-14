a3d4d846888214ac8d489b0c096e30c13b839776
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-11-14 15:30:25 +0530

    ex 02 : versioning the API's

--- Backend/Part Two/BE2.8_CW/index.js
+++ Backend/Part Two/BE2.8_CW/index.js
@@ -1,223 +1,226 @@
 const mongoose = require('mongoose')
-const express=require('express')
-const app=express();
+const express = require('express')
+const app = express();
 app.use(express.json());
 require('./db')
-const User=require('./models/user')
+const User = require('./models/user')
 
-app.get("/",(req,res)=>{
+const v1Routes = require("./routes/v1.routes")
+app.use("/api/v1", v1Routes)
+
+app.get("/", (req, res) => {
   res.send("Connecting User Database with API's")
 })
 
 
 //user signup API
-app.post("/signup",async(req,res)=>{
-  try{
-    const addUser=await signup(req.body);
-    if(addUser){
-      res.json({message:"User created successfully",user:addUser})
-    }else{
-      res.status(404).json({error:"Error adding user"})
+v1Routes.post("/signup", async (req, res) => {
+  try {
+    const addUser = await signup(req.body);
+    if (addUser) {
+      res.json({ message: "User created successfully", user: addUser })
+    } else {
+      res.status(404).json({ error: "Error adding user" })
     }
-  }catch(error){
-    res.status(500).json({error:"Failed signing up user"})
+  } catch (error) {
+    res.status(500).json({ error: "Failed signing up user" })
   }
 })
 
-async function signup(userDetails){
-  try{
-  const user=new User(userDetails)
-  const userSaved=await user.save();
-  console.log("New user created ",userSaved);
-  return user;
-  }catch(e){
-    console.log("Error creating user ",e)
+async function signup(userDetails) {
+  try {
+    const user = new User(userDetails)
+    const userSaved = await user.save();
+    console.log("New user created ", userSaved);
+    return user;
+  } catch (e) {
+    console.log("Error creating user ", e)
   }
 }
 
 //user login API
-app.post("/login",async(req,res)=>{
-  try{
-    const loginUser=await login(req.body.email,req.body.password);
-    if(loginUser){
-      res.json({message:`User loggedin succesfully with the username : ${loginUser.username}`,user:loginUser})
-    }else{
-      res.status(404).json({error:"Login failed,invalid credentials."})
+v1Routes.post("/login", async (req, res) => {
+  try {
+    const loginUser = await login(req.body.email, req.body.password);
+    if (loginUser) {
+      res.json({ message: `User loggedin succesfully with the username : ${loginUser.username}`, user: loginUser })
+    } else {
+      res.status(404).json({ error: "Login failed,invalid credentials." })
     }
-  }catch(error){
-    res.status(500).json({error:"Failed logging in user"})
+  } catch (error) {
+    res.status(500).json({ error: "Failed logging in user" })
   }
 })
 
-async function login(userEmail,userPassword){
-  try{
-    const findUser=await User.findOne({email:userEmail})
-    
+async function login(userEmail, userPassword) {
+  try {
+    const findUser = await User.findOne({ email: userEmail })
+
     // const findPassword=await User.findOne({password:userPassword});
     // console.log(findUser.password)
-    if(findUser && findUser.password===userPassword ){
+    if (findUser && findUser.password === userPassword) {
       console.log("User logged in successfully")
-          return findUser
-    }else{
+      return findUser
+    } else {
       console.log("Invalid Credentials")
     }
-  }catch(e){
-    throw(e);
+  } catch (e) {
+    throw (e);
   }
 }
 
 //changing password API
-app.post("/change-password",async(req,res)=>{
-  try{
-    const updatePassword=await changePassword(req.body.email,req.body.password,req.body.newPassword)
-    
-    if(updatePassword){
-      res.json({message:"Password changed for the user",user:updatePassword})
-    }else{
-      res.status(401).json({error:"Failed to verify existing user,Invalid Credentials."})
+v1Routes.post("/change-password", async (req, res) => {
+  try {
+    const updatePassword = await changePassword(req.body.email, req.body.password, req.body.newPassword)
+
+    if (updatePassword) {
+      res.json({ message: "Password changed for the user", user: updatePassword })
+    } else {
+      res.status(401).json({ error: "Failed to verify existing user,Invalid Credentials." })
     }
-  }catch(error){
-    res.status(500).json({error:"Failed to update the password"})
+  } catch (error) {
+    res.status(500).json({ error: "Failed to update the password" })
   }
 })
-async function changePassword(email,password,newPassword){
-  try{
-    const user=await User.findOne({email})
- 
-    if(user && user.password===password){
+async function changePassword(email, password, newPassword) {
+  try {
+    const user = await User.findOne({ email })
+
+    if (user && user.password === password) {
       console.log("Password Matched,Now updating new password ");
-     
-      user.password=newPassword;
-      const updatedUser=await user.save();
-      console.log("Password Changed for the User ",updatedUser);
+
+      user.password = newPassword;
+      const updatedUser = await user.save();
+      console.log("Password Changed for the User ", updatedUser);
       return updatedUser
-      
-    }else{
+
+    } else {
       console.log("Previous password does not matches");
     }
-  
-     
-  }catch(e){
-    console.log("Error finding user : ",e);
+
+
+  } catch (e) {
+    console.log("Error finding user : ", e);
   }
 }
 
 
 //updating profile picture API
-app.post("/update-profile-picture",async(req,res)=>{
-  try{
-    const {email,password,newPicUrl}=req.body;
-    const updatedUser=await updateProfilePicUrl(email,password,newPicUrl)
-    if(updatedUser){
-      res.json({message:"Update profile pic url for the user",user:updatedUser})
-    }else{
-      res.status(401).json({error:"Invalid Credentials,Could not update profile pic "})
+v1Routes.post("/update-profile-picture", async (req, res) => {
+  try {
+    const { email, password, newPicUrl } = req.body;
+    const updatedUser = await updateProfilePicUrl(email, password, newPicUrl)
+    if (updatedUser) {
+      res.json({ message: "Update profile pic url for the user", user: updatedUser })
+    } else {
+      res.status(401).json({ error: "Invalid Credentials,Could not update profile pic " })
     }
-  }catch(error){
-    res.status(500).json({error:"Failed to update profile pic url"})
+  } catch (error) {
+    res.status(500).json({ error: "Failed to update profile pic url" })
   }
 })
 
-async function updateProfilePicUrl(email,password,newProfilePictureUrl){
+async function updateProfilePicUrl(email, password, newProfilePictureUrl) {
   //no need of password validation
-  try{
-    const user=await User.findOne({email})
-    if(user && user.password===password){
+  try {
+    const user = await User.findOne({ email })
+    if (user && user.password === password) {
       console.log("Password Matched Succesfully,now updating profile pic url")
-      user.profilePictureUrl=newProfilePictureUrl;
-      const savePicUrl=await user.save();
-      console.log("Updated profile pic successfully ",savePicUrl);
+      user.profilePictureUrl = newProfilePictureUrl;
+      const savePicUrl = await user.save();
+      console.log("Updated profile pic successfully ", savePicUrl);
       return user
       //doesn't matter if I return user or savedUser,it would be the smae thing.
-    }else{
+    } else {
       console.log("Enter correct password")
     }
-  }catch(e){
-    console.log("Error updating profile pic url ",e);
+  } catch (e) {
+    console.log("Error updating profile pic url ", e);
   }
 }
 
 
 //updating contact details API
-app.post("/update-contact/:email",async(req,res)=>{
-  try{
-    const email=req.params.email
+v1Routes.post("/update-contact/:email", async (req, res) => {
+  try {
+    const email = req.params.email
     // console.log(req.params.email);
-    const {contactNumber}=req.body
+    const { contactNumber } = req.body
     // console.log(contactNumber);
-    const updatedContact=await updateContactDetails(email,contactNumber)
-    if(updatedContact){
-      res.json({message:"Updated contact number successfully",user:updatedContact})
-    }else{
-      res.status(404).json({error:"Cannot update contact details"})
+    const updatedContact = await updateContactDetails(email, contactNumber)
+    if (updatedContact) {
+      res.json({ message: "Updated contact number successfully", user: updatedContact })
+    } else {
+      res.status(404).json({ error: "Cannot update contact details" })
     }
-  }catch(error){
-    res.status(500).json({error:"Failed to updated contact details for the user"})
+  } catch (error) {
+    res.status(500).json({ error: "Failed to updated contact details for the user" })
   }
 })
-async function updateContactDetails(email,newContactNumber){
-  try{
-    const user=await User.findOne({email});
-    if(user){
+async function updateContactDetails(email, newContactNumber) {
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
       console.log("Updating contact number");
-      user.phoneNumber=newContactNumber;
-      const saveUser=await user.save();
-      console.log("Updated Contact Number Successfully ",saveUser);
+      user.phoneNumber = newContactNumber;
+      const saveUser = await user.save();
+      console.log("Updated Contact Number Successfully ", saveUser);
       return saveUser
-    }else{
+    } else {
       console.log("User not found")
     }
-  }catch(e){
-    console.log("Error updating contact details ",e);
+  } catch (e) {
+    console.log("Error updating contact details ", e);
   }
 }
 
 //finding user by phone number API
-app.get("/users/phone/:phoneNumber",async(req,res)=>{
-  try{
-    const phoneNumber=req.params.phoneNumber;
-    const findUser=await findUserByPhoneNumber(phoneNumber)
-    if(findUser){
-      res.json({message:`Found user with the phone number ${phoneNumber}`,user:findUser})
-    }else{
-      res.status(404).json({error:"Cannot find user"})
+v1Routes.get("/users/phone/:phoneNumber", async (req, res) => {
+  try {
+    const phoneNumber = req.params.phoneNumber;
+    const findUser = await findUserByPhoneNumber(phoneNumber)
+    if (findUser) {
+      res.json({ message: `Found user with the phone number ${phoneNumber}`, user: findUser })
+    } else {
+      res.status(404).json({ error: "Cannot find user" })
     }
-  }catch(error){
+  } catch (error) {
     // throw error
-    res.status(500).json({error:"Failed to find user"})
+    res.status(500).json({ error: "Failed to find user" })
   }
 })
 
-async function findUserByPhoneNumber(phoneNumber){
-  try{
-    const user=await User.findOne({phoneNumber});
-    if(user){
-      console.log("User found succeffully with username",user.username);
+async function findUserByPhoneNumber(phoneNumber) {
+  try {
+    const user = await User.findOne({ phoneNumber });
+    if (user) {
+      console.log("User found succeffully with username", user.username);
       return user;
-    }else{
+    } else {
       console.log("Sorry,user not found ");
     }
-  }catch(e){
-    console.log("Error finding phone number ",e)
+  } catch (e) {
+    console.log("Error finding phone number ", e)
   }
 }
 
 
 
 
-async function addUser(){
-  try{
-  const newUser=new User({
-    email:'xyz@gmail.com',
-    password:"xyz123",
-    profilePictureUrl:"randomxyz.com",
-    username:"xyz",
-    nickname:"exwhyzee"
-  })
-  const userSaved=await newUser.save();
-    console.log("User saved successfully ",userSaved);
-  }catch(e){
-    console.log("Error creating user ",e)
+async function addUser() {
+  try {
+    const newUser = new User({
+      email: 'xyz@gmail.com',
+      password: "xyz123",
+      profilePictureUrl: "randomxyz.com",
+      username: "xyz",
+      nickname: "exwhyzee"
+    })
+    const userSaved = await newUser.save();
+    console.log("User saved successfully ", userSaved);
+  } catch (e) {
+    console.log("Error creating user ", e)
   }
 }
 // addUser();
@@ -234,18 +237,18 @@ async function addUser(){
 
 
 
-async function readAllUsers(){
-  try{
-    const findUsers=await User.find({});
-    console.log("All users ",findUsers)
-  }catch(e){
-    console.log("Error finding users ",e);
+async function readAllUsers() {
+  try {
+    const findUsers = await User.find({});
+    console.log("All users ", findUsers)
+  } catch (e) {
+    console.log("Error finding users ", e);
   }
 }
 // readAllUsers()
 
-const PORT=process.env.PORT || 3000
-app.listen(PORT,()=>{
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 })
 
@@ -262,7 +265,7 @@ app.listen(PORT,()=>{
 
 //Global Middleware
 
-app.use((err,req,res,next)=>{
+app.use((err, req, res, next) => {
   console.log(err.stack);
-  res.status(500).json({error:"Something went wrong"})
+  res.status(500).json({ error: "Something went wrong" })
 })
---
+++ Backend/Part Two/BE2.8_CW/routes/v1.routes.js
@@ -0,0 +1,6 @@
+const express = require("express")
+const router = express.Router()
+const app = express()
+app.use(express.json())
+
+module.exports = router
